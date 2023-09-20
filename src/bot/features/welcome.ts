3f9198304b0d023f83d27096c914b1f48da2b811
Maxim Chernyshov <dsgnrvd@gmail.com>
2023-09-20 10:44:50 +0400

    use pnpm, change /start command

--- src/bot/features/welcome.ts
+++ src/bot/features/welcome.ts
@@ -1,4 +1,4 @@
-import { Composer } from "grammy";
+import { Composer, InlineKeyboard } from "grammy";
 import type { Context } from "#root/bot/context.js";
 import { logHandle } from "#root/bot/helpers/logging.js";
 
@@ -6,8 +6,15 @@ const composer = new Composer<Context>();
 
 const feature = composer.chatType("private");
 
-feature.command("start", logHandle("command-start"), (ctx) => {
-  return ctx.reply(ctx.t("welcome"));
+const frontendUrl = process.env.FRONTEND_URL;
+const urlCreator = (endpoint: string) => frontendUrl + endpoint;
+
+feature.command("start", logHandle("command-start"), async (ctx) => {
+  const inlineKeyboard = new InlineKeyboard()
+    .webApp("Main page", urlCreator("/"))
+    .webApp("Another page", urlCreator("/another-page"));
+
+  await ctx.reply(ctx.t("welcome"), { reply_markup: inlineKeyboard });
 });
 
 export { composer as welcomeFeature };
